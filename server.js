fee3f092b5656ed31a732b4293572d98133d6cae
krishnarao <krishna.r@simpragma.com>
2022-08-11 18:26:44 +0530

    day2

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-
+var cors = require("cors");
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+// use it before all route definitions
+app.use(cors({ origin: "http://localhost:3000" }));
 // app.use('/api/goals', require('./routes/goalRoutes'));
 app.use("/api/users", require("./routes/userRoutes"));
 
